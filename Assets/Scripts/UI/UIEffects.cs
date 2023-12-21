69306d4200462a195d33bef8f01bcbab65ab75eb
MrSoda36 <trystan.perello@outlook.fr>
2023-12-21 16:23:24 +0100

    Remove 'cheat code' to activate UI Effect

--- Assets/Scripts/UI/UIEffects.cs
+++ Assets/Scripts/UI/UIEffects.cs
@@ -15,15 +15,6 @@ public class UIEffects : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
         defaultScale = this.transform.localScale;
     }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Enlarge();
-        }
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            Shrink();
-        }
-    }
-
     void Enlarge() {
         Vector3 enlarge = defaultScale * 1.2f;
         this.transform.DOScale(enlarge, 0.05f);
