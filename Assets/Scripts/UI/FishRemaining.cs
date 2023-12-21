24c805a920e7f4a301f25618ec09f5af014bd97a
NayLiiac <levoyer.kylian24@gmail.com>
2023-12-21 14:25:48 +0100

    correctif fish remaining

--- Assets/Scripts/UI/FishRemaining.cs
+++ Assets/Scripts/UI/FishRemaining.cs
@@ -6,6 +6,11 @@ public class FishRemaining : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI fishRemainingText;
 
+    void Start()
+    {
+       fishRemainingText.text = FishingObjectsList.Instance.beachObjects.Count.ToString() + " Remaining(s)";
+    }
+
     public void SetRemainingFish(List<FishScriptableObjects> list)
     {
         fishRemainingText.text = list.Count.ToString() + " Remaining(s)";
